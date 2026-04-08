a1f6353caf7daadeaa23f91e000f7023d0888eaf
carlosmateusg <ingcarlos.mateus@gmail.com>
2026-04-07 20:37:16 -0500

    feat: listado y mostrar numeros

--- sesion_1.dart
+++ sesion_1.dart
@@ -1,4 +1,7 @@
 void main(){
   String? nombre = "Carlos";
   print("Hola $nombre bienvenido");
+  //mostrando el nombre del usuario 
+  List<int> numeros= [1,2,3,4,5,6,7,8,9,10];
+  numeros.forEach(print);
 }
