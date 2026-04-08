67e854c8d6e1a605b29a4543e5bb5cc345decfe8
carlosmateusg <ingcarlos.mateus@gmail.com>
2026-04-07 20:48:00 -0500

    arreglado  login

--- sesion_1.dart
+++ sesion_1.dart
@@ -4,4 +4,9 @@ void main(){
   //mostrando el nombre del usuario 
   List<int> numeros= [1,2,3,4,5,6,7,8,9,10];
   numeros.forEach(print);
+  Map<String,String> datosSesion={
+    "loginUser": "CarlosM",
+    "loginPass": "1234"
+  };
+  print(datosSesion["loginUser"]);
 }
